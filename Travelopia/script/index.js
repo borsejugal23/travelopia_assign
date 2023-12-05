4776a55ddda5854d59217f2553ee12b30be91cf9
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2023-12-05 15:38:39 +0530

    assignment in progress

---
+++ Travelopia/script/index.js
@@ -0,0 +1,19 @@
+function toggleDescription() {
+    var description = document.getElementById("description");
+    var showMoreText = document.getElementById("showBTN");
+
+    if (description.style.display === "none" || description.style.display === "") {
+        description.style.display = "block";
+        showMoreText.textContent = "Show Less";
+    } else {
+        description.style.display = "none";
+        showMoreText.textContent = "Show More";
+    }
+}
+
+window.onload = function() {
+    var description = document.getElementById("description");
+    description.style.display = "none";
+};
+
+document.getElementById("showBTN").addEventListener("click", toggleDescription);
